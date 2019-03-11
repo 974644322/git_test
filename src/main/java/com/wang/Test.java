a927a1dffb8333b3a7ce50f631a41bb16793145e
wangjixin <13572061584@163.com>
2019-03-11 16:34:52 +0800

    tijiao

---
+++ src/main/java/com/wang/Test.java
@@ -0,0 +1,12 @@
+package com.wang;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+    }
+}
